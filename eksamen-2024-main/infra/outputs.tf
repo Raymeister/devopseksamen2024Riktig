3e72c8ac3993d4b237c7ae4e69d040296c619218
Reidar Strand <62259315+Raymeister@users.noreply.github.com>
2024-11-15 02:42:12 +0100

    Update outputs.tf

--- eksamen-2024-main/infra/outputs.tf
+++ eksamen-2024-main/infra/outputs.tf
@@ -10,5 +10,5 @@ output "lambda_function_name" {
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.image_storage.bucket
+  value       = data.aws_s3_bucket.image_storage.bucket  
 }
