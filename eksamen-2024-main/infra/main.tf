d54b2e3987803a3867e6ba6a45e1d8e888f8816f
Reidar Strand <62259315+Raymeister@users.noreply.github.com>
2024-11-15 02:24:32 +0100

    Update main.tf
    tester

--- eksamen-2024-main/infra/main.tf
+++ eksamen-2024-main/infra/main.tf
@@ -17,17 +17,17 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# S3 bucket for storing generated images
+#for commit change
 resource "aws_s3_bucket" "image_storage" {
   bucket = "pgr301-couch-explorers"
 }
 
-# SQS queue for asynchronous message processing
+
 resource "aws_sqs_queue" "image_generation_queue" {
   name = "image_generation_queue"
 }
 
-# IAM role for Lambda function
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_sqs_integration_role"
   assume_role_policy = jsonencode({
@@ -44,7 +44,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# IAM policy for S3 and SQS access
+
 resource "aws_iam_policy" "lambda_policy" {
   name = "lambda_sqs_s3_access_policy"
   policy = jsonencode({
@@ -79,13 +79,13 @@ resource "aws_iam_policy" "lambda_policy" {
   })
 }
 
-# Attach policy to IAM role
+
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
-# Lambda function for processing SQS messages
+
 resource "aws_lambda_function" "image_generator" {
   function_name = "ImageGenerationFunction"
   role          = aws_iam_role.lambda_role.arn
@@ -93,7 +93,7 @@ resource "aws_lambda_function" "image_generator" {
   runtime       = "python3.9"
   timeout       = 30
 
-  # Replace with the actual path to your deployment package (zip file with the Lambda code)
+
   filename         = "lambda_sqs.zip"
   source_code_hash = filebase64sha256("lambda_sqs.zip")
 
@@ -106,7 +106,7 @@ resource "aws_lambda_function" "image_generator" {
   depends_on = [aws_iam_role_policy_attachment.lambda_policy_attachment]
 }
 
-# Lambda trigger for SQS
+
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.image_generation_queue.arn
   function_name    = aws_lambda_function.image_generator.arn
